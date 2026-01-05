12530229c30485f123ccef4a2629bf4001a31064
Supriya <supriyaaa.926@gmail.com>
2026-01-05 10:04:58 +0530

    Add Python data types practice code to PS01.py

--- python/practice/M01_Language_and_Programming_Basics/S01/PS01.py
+++ python/practice/M01_Language_and_Programming_Basics/S01/PS01.py
@@ -1,2 +1,27 @@
 user_name="Alice"
 print(user_name)
+"""
+Data types:
+1.fundamental
+- int
+- float 
+-complex    (a+bi in python a+bj)
+-Boolean
+_String
+
+2.Collection
+- list
+- tuple
+- set
+- dictionary
+"""
+x=10
+y=3.14
+z=2+3j
+print(type(x))
+print(type(y))
+print(type(z))
+f1=12e1
+f2=5E3
+print(f1,type(f1))
+print(f2,type(f2))
